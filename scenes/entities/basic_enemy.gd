b3c99bba1ba19efbb480a7ed457accf3e356e27a
Olivia Russell <russell.olivia@gmail.com>
2026-02-14 15:00:10 -0800

    basic enemies

--- scenes/entities/basic_enemy.gd
+++ scenes/entities/basic_enemy.gd
@@ -1,13 +1,28 @@
-extends Area2D
+extends CharacterBody2D
 
+var direction = Vector2.DOWN
+var speed = 100
 
-
+var closest_destination = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	#position += direction * speed
+	#for dest in closest_destinations:
+	if closest_destination != null:
+		var dir = (closest_destination.position - position).normalized()
+		velocity = dir * speed
+		move_and_slide()
+
+# I want them to travel to the center of the map by default, then go and stay at their nearest node
+func _on_detection_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("end_positions") and closest_destination == null:
+		print("adding area ", area)
+		closest_destination = area
+	#if area.is_in_group("end_positions") and area not in closest_destinations:
+	#	print("adding area")
+	#	closest_destinations.append(area)
--- scenes/levels/level_1/level_1.gd
+++ scenes/levels/level_1/level_1.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+@onready var animal_on_path = preload("res://scenes/levels/level_1/level_1_animal_path.tscn")
+@onready var enemy = preload("res://scenes/entities/basic_enemy.tscn")
+
 @export var total_spawn_limit = 5
 @export var total_animals_spawned = 0
 @export var animals_that_survived = 0
 @export var animals_that_died = 0
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	check_level_end()
 
 func register_success():
@@ -18,7 +21,17 @@ func check_level_end():
 	if animals_that_died + animals_that_survived >= total_spawn_limit:
 		print("LEVEL DONE!")
 
-func _on_path_spawner_new_spawn() -> void:
+func _on_animal_timer_timeout() -> void:
+	var new_animal_path = animal_on_path.instantiate()
+	$PathSpawner.add_child(new_animal_path)
 	total_animals_spawned += 1
+	
 	if total_animals_spawned >= total_spawn_limit:
-		$PathSpawner/Timer.stop()
+		$PathSpawner/AnimalTimer.stop()
+
+func _on_enemy_timer_timeout() -> void:
+	var new_enemy = enemy.instantiate() as CharacterBody2D
+	$EnemySpawner.add_child(new_enemy)
+	# set new position
+	var position_marker = $EnemySpawner/EnemyStartPositions.get_children().pick_random() as Marker2D
+	new_enemy.position = position_marker.position
